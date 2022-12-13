eb51f347aa397b328696fdb82d59c7a5dfd0e973
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-14 02:32:05 +0530

    exam time - maintaining the streak

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have clicked the button this many :',
+              'You have clicked the button this many times:',
             ),
             Text(
               '$_counter',
