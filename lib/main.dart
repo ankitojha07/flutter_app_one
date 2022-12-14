9407203c91eea5b264b0960cbd19dd3ff2b7d939
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-15 00:27:56 +0530

    incremented

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Incremented',
         child: const Icon(Icons.add),
       ),
     );
