9f374315a6f5ddb47397d7f23102bdab290a68a7
Ankit Ojha <ankitojha1409@gmail.com>
2022-12-16 19:18:51 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_one/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Home Page || Demo app'),
+      home: const Splash(),
     );
   }
 }
